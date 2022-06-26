71e223dffde70bd4ae14714f0de21cedf29bbbb1
Jenita2022 <105583253+Jenita2022@users.noreply.github.com>
2022-06-26 12:33:25 +0530

    Delete runners directory

--- runners/CreateLeadRunner.java
+++
@@ -1,8 +0,0 @@
-package runners;
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features="src/test/java/features/CreateLead.feature", glue="steps", monochrome = true, publish = true)
-public class CreateLeadRunner extends AbstractTestNGCucumberTests{
-}
